f8597ddeb13bcb62ff6cea43ce508b2e34ad0bef
Amanda Wright <akhawkins1@hotmail.com>
2023-07-31 13:53:55 -0400

    Initial upload to github

---
+++ MyTOWN/Models/Events.cs
@@ -0,0 +1,18 @@
+﻿namespace MyTOWN.Models
+{
+    public class Events
+    {
+        public int Id { get; set; }
+        public string Date { get; set; }
+
+        public string Time { get; set; }
+
+        public string Description { get; set; }
+
+        public string Address { get; set; }
+
+        public string Contact { get; set; }
+
+        public int UserProfileId { get; set; }
+    }
+}
---
+++ MyTOWN/Repositories/BaseRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace MyTOWN.Repositories
+{
+  
+        public abstract class BaseRepository //abstract means BaseRepo can only be used by inheritance. 
+        {
+            private readonly string _connectionString;
+
+            public BaseRepository(IConfiguration configuration)
+            {
+                _connectionString = configuration.GetConnectionString("DefaultConnection");
+            }
+
+            protected SqlConnection Connection //protected means it is only available to child classes but not any other code. 
+            {
+                get
+                {
+                    return new SqlConnection(_connectionString);
+                }
+            }
+        }
+    }
+
